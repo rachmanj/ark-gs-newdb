0babe34fc1dedcac7a70c8b4434c006ac9e9ee19
rachmanj <rachmanj@gmail.com>
2025-07-23 08:56:50 +0800

    fix migi export issue

--- app/Exports/MigiExport.php
+++ app/Exports/MigiExport.php
@@ -3,15 +3,48 @@
 namespace App\Exports;
 
 use App\Models\Migi;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class MigiExport implements FromCollection
+class MigiExport implements FromQuery, WithHeadings, WithChunkReading
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    use Exportable;
+
+    public function headings(): array
     {
-        return Migi::all();
+        return [
+            'ID',
+            'Posting Date',
+            'Doc Type',
+            'Doc No',
+            'Project Code',
+            'Dept Code',
+            'Item Code',
+            'Qty',
+            'UOM',
+            'Batch',
+            'Created At',
+            'Updated At'
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function query()
+    {
+        // Filter for current month only
+        return Migi::query()->whereMonth('posting_date', now()->month)
+            ->whereYear('posting_date', now()->year);
+    }
+
+    /**
+     * @return int
+     */
+    public function chunkSize(): int
+    {
+        return 1000;
     }
 }
--- app/Exports/MigiExportThisYear.php
+++ app/Exports/MigiExportThisYear.php
@@ -3,15 +3,47 @@
 namespace App\Exports;
 
 use App\Models\Migi;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class MigiExportThisYear implements FromCollection
+class MigiExportThisYear implements FromQuery, WithHeadings, WithChunkReading
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    use Exportable;
+
+    public function headings(): array
     {
-        return Migi::all();
+        return [
+            'ID',
+            'Posting Date',
+            'Doc Type',
+            'Doc No',
+            'Project Code',
+            'Dept Code',
+            'Item Code',
+            'Qty',
+            'UOM',
+            'Batch',
+            'Created At',
+            'Updated At'
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function query()
+    {
+        // Filter for current year only
+        return Migi::query()->whereYear('posting_date', now()->year);
+    }
+
+    /**
+     * @return int
+     */
+    public function chunkSize(): int
+    {
+        return 1000;
     }
 }
